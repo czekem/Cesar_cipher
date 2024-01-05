eda4e9e02f4e0113cbc7fc468c4dc4084fe47b23
czekem <145675402+czekem@users.noreply.github.com>
2024-01-05 12:59:21 +0100

    Create cesar_main.py

---
+++ cesar_main.py
@@ -0,0 +1,68 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+
+print(logo)
+
+
+def cesar(start_text, shift_amount, cipher_direction):
+"""This function allow to code or decode message, depending on user choose."""
+    end_text = ''
+    if cipher_direction == 'decode':
+        shift_amount *= -1
+    for letter in start_text:
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += letter
+    print(f'The {direction} text is {end_text}')
+    
+
+
+
+active = True
+while active == True :
+
+    
+
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+ 
+        
+        
+    if direction == 'encode':  
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        shift = shift % 26
+    if direction == 'decode':
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        shift = shift % 26
+    
+    
+    cesar(start_text = text, shift_amount = shift, cipher_direction= direction)
+    
+    restart = input('if you want to play again write yes, if no, just write no:\n').lower()
+    if restart == 'no':
+        active = False
+        print('Goodbye.')
